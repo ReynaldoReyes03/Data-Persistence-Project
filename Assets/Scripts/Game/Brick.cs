c6340a35e07643ce07cbc3df802592e4d18eca8f
ReynaldoReyes03 <120312433+ReynaldoReyes03@users.noreply.github.com>
2022-12-26 06:19:45 -0500

    Fixed missing details

--- Assets/Scripts/Game/Brick.cs
+++ Assets/Scripts/Game/Brick.cs
@@ -12,13 +12,13 @@ public class Brick : MonoBehaviour {
 
         switch (PointValue) {
             case 1 :
-                block.SetColor("_BaseColor", Color.green);
+                block.SetColor("_BaseColor", Color.white);
                 break;
             case 2:
-                block.SetColor("_BaseColor", Color.yellow);
+                block.SetColor("_BaseColor", new Color(0.75f, 0.75f, 0.75f));
                 break;
             case 5:
-                block.SetColor("_BaseColor", Color.blue);
+                block.SetColor("_BaseColor", new Color(0.33f, 0.31f, 0.32f));
                 break;
             default:
                 block.SetColor("_BaseColor", Color.red);
